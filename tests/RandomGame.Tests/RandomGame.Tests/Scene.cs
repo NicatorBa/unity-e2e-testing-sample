100d2fc79739727c272dfa0cf168d52154894449
NicatorBa <daniel@bartschinski.info>
2020-02-02 22:12:28 +0100

    Extend the test library and define output path

---
+++ tests/RandomGame.Tests/RandomGame.Tests/Scene.cs
@@ -0,0 +1,22 @@
+﻿using Assets.AltUnityTester.AltUnityDriver;
+using fitlibrary;
+
+namespace RandomGame.Tests
+{
+    public class Scene : DoFixture
+    {
+        public bool HasObjectWithName(string name)
+        {
+            try
+            {
+                var unityObject = TestEnvironment.Driver.WaitForObject(By.NAME, name, timeout: 5);
+
+                return unityObject.enabled;
+            }
+            catch (WaitTimeOutException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ tests/RandomGame.Tests/RandomGame.Tests/TestEnvironment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using fitlibrary;
+
+namespace RandomGame.Tests
+{
+    public class TestEnvironment : DoFixture
+    {
+        private static AltUnityDriver _driver;
+
+        internal static AltUnityDriver Driver
+        {
+            get
+            {
+                if (_driver == null)
+                {
+                    throw new Exception($"Start the {nameof(TestEnvironment)} before use.");
+                }
+
+                return _driver;
+            }
+        }
+
+        public void ConnectToGame()
+        {
+            if (_driver != null)
+            {
+                throw new Exception($"{nameof(TestEnvironment)} already started.");
+            }
+
+            _driver = new AltUnityDriver();
+        }
+
+        public void DisconnectFromGame()
+        {
+            Driver.Stop();
+            _driver = null;
+        }
+    }
+}
